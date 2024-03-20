85f6b28d524950a343bae4d67abf39e0bc4b8d14
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-03-20 22:15:36 +0900

    Todoアイテムの追加を実装する/入力内容をTodoリストに表示する

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { element, render } from "./view/html-util.js";
+
 export class App {
   mount() {
     const formElement = document.getElementById("add-button");
     const inputElement = document.getElementById("add-text");
+    const containerElement = document.getElementById("incomplete-list");
+
+    // TodoリストをまとめるList要素
+    const todoListElement = element`<ul></ul>`;
     formElement.addEventListener("click", (event) => {
       console.log(`入力欄の値: ${inputElement.value}`);
+
+      // 追加するTodoアイテムの要素(li要素)を作成する
+      const todoItemElement = element`<li>${inputElement.value}</li>`;
+      // TodoアイテムをtodoListElementに追加する
+      todoListElement.appendChild(todoItemElement);
+      // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+      render(todoListElement, containerElement);
+
+      // 入力欄を空文字列にしてリセットする
+      inputElement.value = "";
     });
   }
 }
