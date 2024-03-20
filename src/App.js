61a8fd60f9377c3311b8f9b3f283137d1ad973fd
shunsuke ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-03-20 22:03:29 +0900

    Todoアイテムの追加を実装する/入力内容をコンソールに表示する

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-console.log("App.js: loaded");
 export class App {
-  constructor() {
-    console.log("App initialized");
+  mount() {
+    const formElement = document.getElementById("add-button");
+    const inputElement = document.getElementById("add-text");
+    formElement.addEventListener("click", (event) => {
+      console.log(`入力欄の値: ${inputElement.value}`);
+    });
   }
 }
