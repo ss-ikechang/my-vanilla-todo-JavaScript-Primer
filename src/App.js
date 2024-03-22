89b9cbcb2c8caa4b15d7a5e507973dddcc1a8ed0
Shunsuke Ikegami <ikegami.shunsuke@lib.bbiq.jp>
2024-03-22 12:13:12 +0900

    Appのイベントリスナーを整理する/削除ボタン

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ export class App {
   // 1. TodoListModelの初期化
   #incompleteTodoListModel = new TodoListModel();
   #completeTodoListModel = new TodoListModel();
+  #incompleteTodoListView = new IncompleteTodoListView();
+  #completeTodoListView = new CompleteTodoListView();
+
+  // 削除ボタンコールバック関数
+  // onDeleteTodo(props) {
+  //   const { id } = props;
+  //   this.#incompleteTodoListModel.deleteTodo({
+  //     id: id,
+  //   });
+  // }
+  onDeleteTodo = (props) => {
+    const { id } = props;
+    this.#incompleteTodoListModel.deleteTodo({
+      id: id,
+    });
+  };
 
   mount() {
     const formElement = document.getElementById("add-button");
@@ -33,14 +49,6 @@ export class App {
       );
     };
 
-    // 削除ボタンコールバック関数
-    const onDeleteTodo = (props) => {
-      const { id } = props;
-      this.#incompleteTodoListModel.deleteTodo({
-        id: id,
-      });
-    };
-
     // 戻るボタンコールバック関数
     const onBackTodo = (props) => {
       const { id, title } = props;
@@ -56,15 +64,37 @@ export class App {
       );
     };
 
+    // const handleComplete = (props) => {
+    //   this.onDeleteTodo(props);
+    // };
+    // const handleComplete = function (props) {
+    //   this.onDeleteTodo(props);
+    // };
+
     // 2-1. incompleteTodoListModelの状態が更新されたら表示を更新する
     this.#incompleteTodoListModel.onChange(() => {
       // それぞれのTodoItem要素をtodoListElement以下へ追加する
       const todoItems = this.#incompleteTodoListModel.getTodoItems();
-      const incompleteTodoListView = new IncompleteTodoListView();
-      const todoListElement = incompleteTodoListView.createElement(todoItems, {
-        onCompleteTodo: onCompleteTodo,
-        onDeleteTodo: onDeleteTodo,
-      });
+      const todoListElement = this.#incompleteTodoListView.createElement(
+        todoItems,
+        {
+          onCompleteTodo: onCompleteTodo,
+          onDeleteTodo: this.onDeleteTodo,
+
+          // onDeleteTodo: (props) => {
+          //   this.onDeleteTodo(props);
+          // },
+          // クラス - JavaScript | MDN
+          // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Classes
+          // 静的メソッドやインスタンスメソッドを this の値なしで呼び出した場合、例えばメソッドを変数に割り当ててから呼び出すと、メソッド内部では this の値が undefined になります。
+          // この動作は、 "use strict" ディレクティブが存在しない場合でも同じです。 class 本体の中のコードは常に厳格モードで実行されるからです。
+          // 【JavaScript】アロー関数式を学ぶついでにthisも復習する話 #JavaScript - Qiita
+          // https://qiita.com/mejileben/items/69e5facdb60781927929
+          // アロー関数式で宣言された関数は、宣言された時点で、thisを確定（＝束縛）させてしまうのです。
+
+          // onDeleteTodo: handleComplete,
+        }
+      );
 
       // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
       render(todoListElement, incomlpeteTodoContainerElement);
@@ -74,10 +104,12 @@ export class App {
     this.#completeTodoListModel.onChange(() => {
       // それぞれのTodoItem要素をtodoListElement以下へ追加する
       const todoItems = this.#completeTodoListModel.getTodoItems();
-      const completeTodoListView = new CompleteTodoListView();
-      const todoListElement = completeTodoListView.createElement(todoItems, {
-        onBackTodo: onBackTodo,
-      });
+      const todoListElement = this.#completeTodoListView.createElement(
+        todoItems,
+        {
+          onBackTodo: onBackTodo,
+        }
+      );
 
       // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
       render(todoListElement, comlpeteTodoContainerElement);
